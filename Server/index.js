4ebfc291b475898582388f22d96ab32fd7cd3e7f
Shubhankar Pandey <shubhankarpandey1509@gmail.com>
2026-04-26 03:01:01 +0530

    test route added

--- Server/index.js
+++ Server/index.js
@@ -51,3 +51,8 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server started at port nummber ${PORT}`);
 })
+
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
